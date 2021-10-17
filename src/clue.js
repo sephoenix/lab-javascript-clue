518087f91281acff04fa606d83d19b60c5a47ea8
Carlos Parodi <se.phoenix@gmail.com>
2021-10-17 14:02:38 +0200

    lab cluedo

--- src/clue.js
+++ src/clue.js
@@ -1,29 +1,139 @@
+
 // ITERATION 1
 
 // Suspects Array
 
-const suspectsArray = [];
+const suspectsArray = [ 
+  {
+   firstName: "Jacob",
+   lastName: "Green",
+   occupation: "Entrepreneur",
+   age: 45,
+   description: "He has a lot of connections",
+   image: "https://pbs.twimg.com/profile_images/506787499331428352/65jTv2uC.jpeg" ,
+   color: "green",
+   },
+ 
+   {
+     firstName: "Doctor",
+     lastName: "Orchid",
+     occupation: "Scientist",
+     age: 26,
+     description: "PhD in plant toxicology. Adopted daughter of Mr. Boddy",
+     image: "http://www.radiotimes.com/uploads/images/Original/111967.jpg",
+     color: "white",
+   },
+ 
+   {
+   firstName: "Victor",
+   lastName: "Plum",
+   occupation: "Designer",
+   age: 22,
+   description: "Billionaire video game designer",
+   image: "https://66.media.tumblr.com/ee7155882178f73b3781603f0908617c/tumblr_phhxc7EhPJ1w5fh03_540.jpg",
+   color: "purple",
+   },
+ 
+   {
+   firstName: "Kasandra",
+   lastName: "Scarlet",
+   occupation: "Actor",
+   age: 31,
+   description: "She is an A-list movie star with a dark past",
+   image: "https://www.radiotimes.com/uploads/images/Original/111967.jpg",
+   color: "red",
+   },
+ 
+   {
+   firstName: "Eleanor",
+   lastName: "Peacock",
+   occupation: "Socialité",
+   age: 36,
+   description: "She is from a wealthy family and uses her status and money to earn popularity",
+   image: "https://metrouk2.files.wordpress.com/2016/07/mrs-peacock.jpg",
+   color: "blue",
+   },
+ 
+   {
+   firstName: "Jack",
+   lastName:  "Mustard",
+   occupation: "Retired Football player",
+   age: 62,
+   description: "He is a former football player who tries to get by on his former glory",
+   image: "https://static.independent.co.uk/s3fs-public/thumbnails/image/2016/07/04/08/unspecified-3.jpg",
+   color: "yellow",
+   }];
 
 // Rooms Array
 
-const roomsArray = [];
+let roomsArray = [
+  "Dinning Room",
+  "Conservatory",
+  "Kitchen",
+  "Study",
+  "Library",
+  "Billiard Room",
+  "Lounge",
+  "Ballroom",
+  "Hall",
+  "Spa",
+  "Living Room",
+  "Observatory",
+  "Theater",
+  "Guest House",
+  "Patio"];
 
 // Weapons Array
 
-const weaponsArray = [];
+let weaponsArray = [
+  {name: "rope", weight: 10},
+  {name: "knife", weight: 8},
+  {name: "candlestick", weight: 2},
+  {name: "dumbbell" , weight: 30},
+  {name: "poison", weight: 2},
+  {name: "axe", weight: 15},
+  {name: "bat", weight: 13},
+  {name: "trophy", weight: 25},
+  {name: "pistol", weight: 20}
+  ];
 
 
 // ITERATION 2
 
-function selectRandom() {}
+function selectRandomSuspect (suspectsArray){
+  let suspects = suspectsArray[Math.floor(Math.random()*suspectsArray.length)];
+  return suspects;
+  }
+  selectRandomSuspect(suspectsArray);
 
-function pickMystery() {}
+function selectRandomRoom (roomsArray){
+  let room = roomsArray[Math.floor(Math.random()*roomsArray.length)];
+  return room;
+  }
+  selectRandomRoom(roomsArray);
+
+function selectRandomWeapon (weaponsArray){
+  let weapons = weaponsArray[Math.floor(Math.random()*weaponsArray.length)];
+  return weapons;
+  }
+  selectRandomWeapon(weaponsArray);
+      
+let mystery =[];
+function pickMystery(randomMistery){
+  mystery.push(selectRandomSuspect(suspectsArray));
+  mystery.push(selectRandomRoom(roomsArray));
+  mystery.push(selectRandomWeapon(weaponsArray));
+  return mystery;
+  }
+  pickMystery();
 
 
 // ITERATION 3
 
-function revealMystery() {}
-
+function revealMystery() {
+  return (`${mystery[0].firstName} ${mystery[0].lastName} killed Mr.Boddy using the ${mystery[2].name} in the ${mystery[1]}!`)
+}
+revealMystery()
 
 
 // The following is required to make unit tests work.
